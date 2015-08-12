355aeed85b6f6237badf74d90c5ad746c6aa07b4
John Louie <jlouie95618@gmail.com>
2015-08-11 19:25:18 -0700

    very minor changes, added a css file, and cleaned up the extension.js file; however, most changes are present in the S3 bucket as test.js and test.css

--- js/extension.js
+++ js/extension.js
@@ -1,10 +1,10 @@
 'use strict'; // indicate to use Strict Mode
 
-console.log('EXTENSION: ', $);
-
 var script = $('<script/>');
+var css = $('<link/>');
 script.attr('src', 'https://airtable-webapp-console.s3-us-west-1.amazonaws.com/test.js');
+css.attr('href', 'https://airtable-webapp-console.s3-us-west-1.amazonaws.com/test.css');
+css.attr('rel', 'stylesheet');
+css.attr('type', 'text/css');
 $('head').append(script);
-
-// script.attr('src', 'http://localhost:8000/test.js');
-// $('head').append(script);
+$('head').append(css);
